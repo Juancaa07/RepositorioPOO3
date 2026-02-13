5886b5fd5a4e752c49962eae807007295e935315
Juanca <camijuan2cy@gmail.com>
2026-02-13 17:16:53 -0500

    comandos git

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("PRIMEROS COMANDOS DE GIT");
+        System.out.println("Juan Camilo Collazos (Python)");
+
+        System.out.println("1. COMANDOS DE GIT VISTOS");
+        System.out.println("1. git add");
+        System.out.println("2. git commit -M");
+        System.out.println("3. git push");
+        System.out.println("4. git switch");
 
         }
     }
